0527a71532820553298fb85bda603927a37bb495
Bomb Atomically <ieugenelee@gmail.com>
2021-06-30 09:30:23 -0700

    password length criteria fulfilled

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Assignment code here
+// Assignment code here - Global
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*()";
 
  
 // Get references to the #generate element
@@ -8,10 +12,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
+
+function generatePassword(){
+  var passwordlength = 0; //local
+  passwordlength = parseInt(prompt("Enter Length")); // Convert String to Integer
+  if (passwordlength < 8 || passwordlength > 128) {
+    return "Invalid Password length ... please choose between 8 and 128"
+  } else {
+
+  }
+
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
